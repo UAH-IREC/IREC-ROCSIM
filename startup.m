4b98c71e0b7cea6113dff36b365937e13e405cdf
ajb0035 <ajb0035@uah.edu>
2017-08-05 14:12:01 -0500

    Added "Data" Folder

--- startup.m
+++ startup.m
@@ -2,5 +2,6 @@ addpath('main');
 addpath(fullfile('MatlabTools'));
 addpath(fullfile('MatlabCEA'));
 addpath(fullfile('Supporting Functions'));
+addpath(fullfile('Data'));
 %addpath(fullfile('tdmsSubfunctions'));
 
