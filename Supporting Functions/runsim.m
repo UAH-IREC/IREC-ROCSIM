efebd098460ad4cd9ca4a30dce98fdcd7d0eccaa
moresemicolons <31354662+moresemicolons@users.noreply.github.com>
2017-08-26 16:18:17 -0500

    All parameters as inputs and outputs flight data

--- Supporting Functions/runsim.m
+++ Supporting Functions/runsim.m
@@ -1,4 +1,4 @@
-function max_vals = runsim(Tamb, pamb, MR, expansion_ratio, chamber_pressure)
+function [max_vals, flightdata, forces, Roc, Eng, Prop] = runsim(Conditions, Prop, Eng, Roc)
 %RUNSIM Summary of this function goes here
 %   Tamb:               ambient temperature (K)
 %   Pamb:               ambient pressure (Pa)
@@ -23,24 +23,31 @@ lb2kg = 0.453592; % convert lbs to kg
 lb2N = 4.448; % convert lbs to N
 in2m = 0.0254; % convert in to m
 
-
-if ~exist('Tamb', 'var')
+if ~isfield(Conditions, 'Tamb')
     warning('Ambient temperature not provided. Defaulting to 300 K')
     Tamb = 300; % Ambient Temperature [K] (305K is 90F)
+else
+    Tamb = Conditions.Tamb;
 end
-if ~exist('pamb', 'var')
+if ~isfield(Conditions, 'pamb')
     warning('Ambient pressure not provided. Defaulting to 14.7 psi')
     pamb = 14.7*psi2pa; % Assumed ambient pressure [Pa]
+else
+    pamb = Conditions.pamb;
 end
 
 % Propellant Selection & Properties
-Prop.ox.name = {'NITROUSOXIDE','N2O'}; % Nitrous Oxide
-Prop.f.name = {'ETHANE','C2H6'};
-if ~exist('MR', 'var')
+if ~isfield(Prop, 'ox') || ~isfield(Prop.ox, 'name')
+    warning('Oxidizer not specified. Defaulting to nitrous oxide')
+    Prop.ox.name = {'NITROUSOXIDE','N2O'}; % Nitrous Oxide
+end
+if ~isfield(Prop, 'f') || ~isfield(Prop.f, 'name')
+    warning('Fuel not specified. Defaulting to ethane')
+    Prop.f.name = {'ETHANE','C2H6'};
+end
+if ~isfield(Eng, 'MR')
     warning('Engine mixture ratio not provided. Defaulting to 8')
     Eng.MR = 8;
-else
-    Eng.MR = MR;
 end
 
 Prop.ox.p = CoolProp.PropsSI('P', 'T', Tamb, 'Q', 0, char(Prop.ox.name(1))); % [Pa]
@@ -52,20 +59,20 @@ Prop.f.rho = CoolProp.PropsSI('D', 'T', Tamb, 'Q', 0, char(Prop.f.name(1))); % [
 
 % Assumed Engine Performance Values
 
-if ~exist('chamber_pressure', 'var')
+if ~isfield(Eng, 'pct_psi')
     warning('Combustion chamber pressure not provided. Defaulting to 300 psi')
     Eng.pct_psi = 300; % Chamber total pressure [psia]
-else
-    Eng.pct_psi = chamber_pressure;
 end
 Eng.pct = Eng.pct_psi*psi2pa; % [Pa]
-if ~exist('expansion_ratio', 'var')
+if ~isfield(Eng, 'eps')
     warning('Engine expansion ratio not provided. Defaulting to 4')
     Eng.eps = 4; % Nozzle expansion ratio (Ae/At)
-else
-    Eng.eps = expansion_ratio;
 end
-Eng.cstar_eta = 0.9; % C* Efficiency 
+
+if ~isfield(Eng, 'cstar_eta')
+    warning('Cstar value not provided, defaulting to 0.9')
+    Eng.cstar_eta = 0.9; % C* Efficiency
+end
 
 % Derived Engine Performance Values
 [data] = CEA_Rocket(char(Prop.ox.name(2)),char(Prop.f.name(2)),Eng.pct_psi,Eng.MR,Eng.eps,Tamb,Tamb);
@@ -79,17 +86,41 @@ Eng.Cfp = Eng.eps*((Eng.pe-pamb)/Eng.pct); % Pressure term thrust coefficient
 Eng.Cf = Eng.Cfvac + Eng.Cfp; % Actual thrust coefficient
 
 % Assumed Rocket Parameters
-Roc.Cd = csvread('CdvsM_RASAERO.csv')'; % Courtesy Aaron Hunt
-Roc.d = 6; % 6 diameter of 1st stage [in]
+if ~isfield(Roc, 'Cd')
+    warning('Drag coefficients not provided, using CdvsM_RASAERO.csv');
+    Roc.Cd = csvread('CdvsM_RASAERO.csv')'; % Courtesy Aaron Hunt
+end
+if ~isfield(Roc, 'd')
+    warning('Maximum rocket diameter not specified, assuming 6 in');
+    Roc.d = 6; % 6 diameter of 1st stage [in]
+end
 Roc.A = (pi/4)*Roc.d^2*0.00064516; % Used for drag calc [m^2]
-Roc.TW = 5; % Initial Thrust to Weight Ratio
-Vull = 0.15; % Assumed Tank Ullage %
-Eng.m = 3; % Engine Mass [kg]
-Prop.ox.V = 0.02; % Volume of ox [m^3]
+if ~isfield(Roc, 'TW')
+    warning('Target thrust-to-weight ratio not given, assuming 5');
+    Roc.TW = 5; % Initial Thrust to Weight Ratio
+end
+if ~isfield(Roc, 'tanks') || ~isfield(Roc.tanks, 'Vull')
+    warning('Ullage volume not given, assuming 15%');
+    Vull = 0.15; % Assumed Tank Ullage %
+end
+if ~isfield(Eng, 'm')
+    warning('Engine mass not given, assuming 3 kg');
+    Eng.m = 3; % Engine Mass [kg]
+end
+if isfield(Prop, 'ox') && (isfield(Prop.ox, 'V') || isfield(Prop.ox, 'm'))
+    if isfield(Prop.ox, 'V')
+        Prop.ox.m = Prop.ox.V*Prop.ox.rho; % [kg]
+    else
+        Prop.ox.V = Prop.ox.m / Prop.ox.rho; % m^3
+    end
+else
+    warning('Propellant amounts not specified, defaulting to 20 liters of oxidizer');
+    Prop.ox.V = 0.02; % Volume of ox [m^3]
+    Prop.ox.m = Prop.ox.V*Prop.ox.rho; % [kg]
+end
 
 % Amount of Propellants
 Roc.tank.Vox = Prop.ox.V*(1+Vull); % Amount of liquid oxygen [m^3]
-Prop.ox.m = Prop.ox.V*Prop.ox.rho; % [kg]
 Prop.f.m = Prop.ox.m/Eng.MR; % [kg]
 Prop.f.V = Prop.f.m/Prop.f.rho; % [m^3]
 Roc.tank.Vf = Prop.f.V*(1+Vull); % [m^3]
@@ -101,32 +132,44 @@ Roc.tank.Vf_in3 = Roc.tank.Vf/(1.63871*10^-5); % [in^3]
 % Metal Tanks
 [Roc.tank.lox,Roc.tank.tox,Roc.tank.Wox,Roc.tank.Fox] = WeldedTankCalc(Roc.tank.Vox_in3,Prop.ox.p_psi,Roc.d/2,'Al6061');
 [Roc.tank.lf,Roc.tank.tf,Roc.tank.Wf,Roc.tank.Ff] = WeldedTankCalc(Roc.tank.Vf_in3,Prop.ox.p_psi,Roc.d/2,'Al6061'); 
-Roc.tank.mox = 20*lb2kg; %Roc.tank.Wox*lb2kg; % [kg]
-Roc.tank.mf = 5.27*lb2kg; %Roc.tank.Wf*lb2kg; % [kg]
-% Carbon Fiber tanks
-m_6in_CF_ft = (0.6)*2.766/4; % Carbon Fiber 6 in diameter [kg/ft] 
+Roc.tank.mox = Roc.tank.Wox*lb2kg; % [kg]
+Roc.tank.mf = Roc.tank.Wf*lb2kg; % [kg]
+% Carbon Fiber airframe mass
+if ~isfield(Roc, 'airframe_linear_density')
+    warning('Airframe linear density not given, assuming 0.6 kg/ft');
+    m_6in_CF_ft = (0.6)*2.766/4; % Carbon Fiber 6 in diameter [kg/ft]
+else
+    m_6in_CF_ft = Roc.airframe_linear_density;
+end
 
 Roc.tank.m = Roc.tank.mox + Roc.tank.mf; % Total tank mass [kg]
 
 % Airframe Mass Relationships
-Roc.air.lbody = 1+1+2+2.5; % Estimate of body tube length sections [ft]
-Roc.air.mbody = Roc.air.lbody * m_6in_CF_ft; % [kg]
-Roc.air.mnose = 0.3; % Nose cone [kg]
-Roc.air.mfin = 0.3; % Fins [kg]
-Roc.air.mavi = 1; % Avionics [kg]
-Roc.air.mmisc = 2; % Misc mechanical [kg]
-Roc.air.mrecov = 6; % Recovery [kg]
-Roc.air.m = Roc.air.mbody + Roc.air.mnose + Roc.air.mfin + ...
-      Roc.air.mavi + Roc.air.mmisc + Roc.air.mrecov;
-Roc.mpay = 9 * lb2kg; % Payload Mass [kg]
-% Fluid Tubing etc Mass Relationships
-Roc.tank.mvalves = 6*0.5 * lb2kg; % Estimate 6 valves, 0.5 lb/valve [kg]
-Roc.tank.mline = 2; % 5 ft/lb, 17 ft & fittings [kg]
+if ~isfield(Roc, 'minert')
+    warning('Rocket inert mass not specified, assuming many values');
+    warning('Assuming mass can affect simulated performance drastically');
+    Roc.air.lbody = 1+1+2+2.5; % Estimate of body tube length sections [ft]
+    Roc.air.mbody = Roc.air.lbody * m_6in_CF_ft; % [kg]
+    Roc.air.mnose = 0.3; % Nose cone [kg]
+    Roc.air.mfin = 0.3; % Fins [kg]
+    Roc.air.mavi = 1; % Avionics [kg]
+    Roc.air.mmisc = 2; % Misc mechanical [kg]
+    Roc.air.mrecov = 6; % Recovery [kg]
+    Roc.air.m = Roc.air.mbody + Roc.air.mnose + Roc.air.mfin + ...
+          Roc.air.mavi + Roc.air.mmisc + Roc.air.mrecov;
+    Roc.mpay = 9 * lb2kg; % Payload Mass [kg]
+    % Fluid Tubing etc Mass Relationships
+    Roc.tank.mvalves = 6*0.5 * lb2kg; % Estimate 6 valves, 0.5 lb/valve [kg]
+    Roc.tank.mline = 2; % 5 ft/lb, 17 ft & fittings [kg]
 
-% Inert mass of Rocket [kg]
-mmarg = 0.15; % Mass margin %
-Roc.minert = (Roc.tank.m + Roc.air.m + Eng.m + Roc.tank.mvalves + ...
-    Roc.tank.mline + Roc.mpay) * (1+mmarg); % Inert mass of rocket[kg]
+    % Inert mass of Rocket [kg]
+    mmarg = 0.15; % Mass margin %
+    Roc.minert = (Roc.tank.m + Roc.air.m + Eng.m + Roc.tank.mvalves + ...
+        Roc.tank.mline + Roc.mpay) * (1+mmarg); % Inert mass of rocket[kg]
+else
+    Roc.minert = Roc.minert + Eng.m;
+end
+    
 % Wet Mass and More Derived Performance
 Roc.mwet = Prop.m + Roc.minert; % Wet mass of rocket [kg]
 Roc.Mprop = Prop.m/Roc.mwet; % Propellant mass ratio
@@ -150,21 +193,38 @@ Eng.Imp_lb = Eng.thrustGL_lb*Eng.tburn; % Calculated impulse [lb-s]
 Eng.Isp = Eng.thrustGL / (g*Eng.mdot); % Specific Impulse [s]
 
 % Non Sim Parameters
-Eng.Lstar = 45*in2m; % Characteristic chamber length [m]
+if ~isfield(Eng, 'Lstar')
+    warning('Characteristic chamber length not given, assuming 45 in');
+    Eng.Lstar = 45*in2m; % Characteristic chamber length [m]
+end
 Eng.Vc = Eng.Lstar*Eng.At;
 Eng.Vc_in3 = Eng.Vc / in2m^3;
-Eng.epsc = 7; % Chamber contraction ratio (From LRE Book p. 73)
+if ~isfield(Eng, 'epsc')
+    warning('Chamber contraction ratio not given, assuming 7');
+    Eng.epsc = 7; % Chamber contraction ratio (From LRE Book p. 73)
+end
 Eng.Ac = Eng.epsc * Eng.At; % Area of chamber [m^2]
 Eng.Ac_in2 = Eng.Ac / in2m^2; % [in]
 Eng.dc_in = (Eng.Ac_in2*4/pi)^0.5; % Chamber diameter [in]
-Eng.thetac = 45; % Chamber/throat contraction angle [deg]
+if ~isfield(Eng, 'thetac')
+    warning('Chamber-to-throat contraction angle not specified, assuming 45 degrees');
+    Eng.thetac = 45; % Chamber/throat contraction angle [deg]
+end
 Eng.Lc_in = ((Eng.Vc_in3/Eng.At_in2)-(1/3)*(Eng.At_in2/pi)^0.5 * cotd(Eng.thetac)*(Eng.epsc^1/3 - 1))/Eng.epsc; % Chamber length [in]
-Eng.alpn = 15; % Cone nozzle angle [deg]
+if ~isfield(Eng, 'alpn')
+    warning('Nozzle cone angle not specified, assuming 15 degrees');
+    Eng.alpn = 15; % Cone nozzle angle [deg]
+end
 Eng.Ln_in = ((Eng.de_in-Eng.dt_in)/2) / tand(Eng.alpn); % Cone nozzle length [in]
 
 %% Simulation Section
 options = simset('SrcWorkspace','current');
-tend = 90; % End time of sim in seconds
+
+if ~isfield(Conditions, 'tend')
+    tend = 90; % End time of sim in seconds
+else
+    tend = Conditions.tend;
+end
 
 
 tic % Record sim computation time
