75e42bf4bca43c0b803e866ac7d6f1df28409b29
moresemicolons <31354662+moresemicolons@users.noreply.github.com>
2017-08-26 21:47:46 -0500

    Simulation results integrated into Excel
    Simulation results and calculated parameters are now saved into a time-stamped sheet in the Excel configuration file

--- LaunchControl.m
+++ LaunchControl.m
@@ -7,7 +7,7 @@ startup % run startup script for CEA and Coolprop
 %pressure (psi)
 
 %% Simulation Conditions
-atm_conditions.garbage = 0;
+atm_conditions = [];
 atmoptions = readtable('simconfig.xlsx', 'Sheet', 'Simulation Conditions');
 
 [val, type] = param_from_table(atmoptions, 'Ambient pressure', 1);
@@ -21,9 +21,9 @@ if type == ParameterType.SingleValue
 end
 
 %% Propellant Options
-prop_params.garbage = 0;
-prop_params.ox.garbage = 0;
-prop_params.f.garbage = 0;
+prop_params = [];
+% prop_params.ox.garbage = 0;
+% prop_params.f.garbage = 0;
 propoptions = readtable('simconfig.xlsx', 'Sheet', 'Propellant Parameters');
 for i = 1:height(propoptions)
     if strcmp(table2cell(propoptions(i, 1)), 'Oxidizer')
@@ -116,3 +116,41 @@ end
 %% Simulation Execution
 
 [max, flightdata, forces, Roc, Eng, Prop] = runsim(atm_conditions, prop_params, engine_params, rocket_params)
+
+%% Output
+
+% From https://www.mathworks.com/matlabcentral/answers/2603-add-a-new-excel-sheet-from-matlab
+% Connect to Excel
+Excel = actxserver('excel.application');
+% Get Workbook object
+WB = Excel.Workbooks.Open(fullfile(pwd, 'simconfig.xlsx'), 0, false);
+Excel.Visible = true;
+% Get Worksheets object
+WS = WB.Worksheets;
+% Add after the last sheet
+WS.Add([], WS.Item(WS.Count));
+WS.Item(WS.Count).Name = ['Results ' strrep(datestr(now()), ':', '-')];
+
+% https://stackoverflow.com/questions/7636567/write-information-into-excel-after-each-loop
+WB.Sheets.Item(WS.Count).Activate();
+Excel.Range('A1:E1').Select();
+Excel.Selection.Value = {'Time (s)', 'Acceleration (m/s)', 'Velocity (m/s)', 'Altitude (m)', 'Mach Number'};
+[rows, cols] = size(flightdata);
+Excel.Range(sprintf('B2:E%i', rows)).Select()
+Excel.Selection.Value = num2cell(flightdata);
+
+% Insert results
+begin_col = 8;
+[~, plusc] = insert_struct_into_excel(max, 'Maximums', Excel, [1, begin_col]);
+begin_col = begin_col + plusc + 2;
+[~, plusc] = insert_struct_into_excel(Prop, 'Propellants', Excel, [1, begin_col]);
+begin_col = begin_col + plusc + 2;
+[~, plusc] = insert_struct_into_excel(Eng, 'Engine', Excel, [1, begin_col]);
+begin_col = begin_col + plusc + 2;
+[~, plusc] = insert_struct_into_excel(Roc, 'Rocket', Excel, [1, begin_col]);
+begin_col = begin_col + plusc + 2;
+
+% Save
+WB.Save();
+% Quit Excel
+Excel.Quit();
