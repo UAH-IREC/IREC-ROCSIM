a6a7cedc2c9138ed96b9c57d30a24ee1ec24b099
Thomas Bennett <trb0023@uah.edu>
2017-09-17 14:16:24 -0500

    Forgot to uncomment stuff

--- LaunchControl.m
+++ LaunchControl.m
@@ -108,44 +108,44 @@ end
 
 %% Output
 
-% % From https://www.mathworks.com/matlabcentral/answers/2603-add-a-new-excel-sheet-from-matlab
-% % Connect to Excel
-% Excel = actxserver('excel.application');
-% % Get Workbook object
-% WB = Excel.Workbooks.Open(fullfile(pwd, 'simconfig.xlsx'), 0, false);
-% Excel.Visible = true;
-% % Get Worksheets object
-% WS = WB.Worksheets;
-% % Add after the last sheet
-% WS.Add([], WS.Item(WS.Count));
-% WS.Item(WS.Count).Name = ['Results ' strrep(datestr(now()), ':', '-')];
-% 
-% % https://stackoverflow.com/questions/7636567/write-information-into-excel-after-each-loop
-% WB.Sheets.Item(WS.Count).Activate();
-% Excel.Range('A1:E1').Select();
-% Excel.Selection.Value = {'Time (s)', 'Acceleration (m/s)', 'Velocity (m/s)', 'Altitude (m)', 'Mach Number'};
-% [rows, ~] = size(flightdata);
-% Excel.Range(sprintf('A2:E%i', rows)).Select();
-% Excel.Selection.Value = num2cell(flightdata);
-% 
-% Excel.Range('F1:H1').Select();
-% Excel.Selection.Value = {'Mass (kg)', 'Drag (N)', 'Thrust (N)'};
-% [rows, ~] = size(forces);
-% Excel.Range(sprintf('F2:H%i', rows)).Select();
-% Excel.Selection.Value = num2cell(forces);
-% 
-% % Insert results
-% begin_col = 11;
-% [~, plusc] = insert_struct_into_excel(max, 'Maximums', Excel, [1, begin_col]);
-% begin_col = begin_col + plusc + 2;
-% [~, plusc] = insert_struct_into_excel(Prop, 'Propellants', Excel, [1, begin_col]);
-% begin_col = begin_col + plusc + 2;
-% [~, plusc] = insert_struct_into_excel(Eng, 'Engine', Excel, [1, begin_col]);
-% begin_col = begin_col + plusc + 2;
-% [~, plusc] = insert_struct_into_excel(Roc, 'Rocket', Excel, [1, begin_col]);
-% begin_col = begin_col + plusc + 2;
-% 
-% % Save
-% WB.Save();
-% % Quit Excel
-% Excel.Quit();
+% From https://www.mathworks.com/matlabcentral/answers/2603-add-a-new-excel-sheet-from-matlab
+% Connect to Excel
+Excel = actxserver('excel.application');
+% Get Workbook object
+WB = Excel.Workbooks.Open(fullfile(pwd, 'simconfig.xlsx'), 0, false);
+Excel.Visible = true;
+% Get Worksheets object
+WS = WB.Worksheets;
+% Add after the last sheet
+WS.Add([], WS.Item(WS.Count));
+WS.Item(WS.Count).Name = ['Results ' strrep(datestr(now()), ':', '-')];
+
+% https://stackoverflow.com/questions/7636567/write-information-into-excel-after-each-loop
+WB.Sheets.Item(WS.Count).Activate();
+Excel.Range('A1:E1').Select();
+Excel.Selection.Value = {'Time (s)', 'Acceleration (m/s)', 'Velocity (m/s)', 'Altitude (m)', 'Mach Number'};
+[rows, ~] = size(flightdata);
+Excel.Range(sprintf('A2:E%i', rows)).Select();
+Excel.Selection.Value = num2cell(flightdata);
+
+Excel.Range('F1:H1').Select();
+Excel.Selection.Value = {'Mass (kg)', 'Drag (N)', 'Thrust (N)'};
+[rows, ~] = size(forces);
+Excel.Range(sprintf('F2:H%i', rows)).Select();
+Excel.Selection.Value = num2cell(forces);
+
+% Insert results
+begin_col = 11;
+[~, plusc] = insert_struct_into_excel(max, 'Maximums', Excel, [1, begin_col]);
+begin_col = begin_col + plusc + 2;
+[~, plusc] = insert_struct_into_excel(Prop, 'Propellants', Excel, [1, begin_col]);
+begin_col = begin_col + plusc + 2;
+[~, plusc] = insert_struct_into_excel(Eng, 'Engine', Excel, [1, begin_col]);
+begin_col = begin_col + plusc + 2;
+[~, plusc] = insert_struct_into_excel(Roc, 'Rocket', Excel, [1, begin_col]);
+begin_col = begin_col + plusc + 2;
+
+% Save
+WB.Save();
+% Quit Excel
+Excel.Quit();
