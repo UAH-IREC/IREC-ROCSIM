899e58281c0760d3ccda04c8ac0ee57c692a7dce
Thomas Bennett <trb0023@uah.edu>
2017-09-17 15:10:57 -0500

    Basic Monte Carlo output
    Monte Carlo now computes a histogram for maximum altitude. Techically useable now, but only barely.

--- LaunchControl.m
+++ LaunchControl.m
@@ -7,7 +7,7 @@ startup % run startup script for CEA and Coolprop
 %pressure (psi)
 
 %READ THESE IN FROM EXCEL LATER
-monte_carlo_iterations = 5;
+monte_carlo_iterations = 190;
 
 %% Simulation Conditions
 atm_conditions = [];
@@ -87,65 +87,73 @@ mode = detect_value_types(atm_conditions, prop_params, engine_params, rocket_par
 results = [];
 
 
-
 startup % run startup script for CEA and Coolprop
 if (mode == 1)
     [max, flightdata, forces, Roc, Eng, Prop] = runsim(atm_conditions, prop_params, engine_params, rocket_params);
 elseif (mode == 2)
-    error('Monte Carlo isn''t quite ready yet. Check back later!');
-    for run = 1:monte_carlo_iterations
+    %error('Monte Carlo isn''t quite ready yet. Check back later!');
+    for runNum = 1:monte_carlo_iterations
+        fprintf('\nOn iteration %.0f of %.0f', runNum, monte_carlo_iterations);
         
         [this_run_atm_conditions, this_run_prop_params, this_run_engine_params, this_run_rocket_params] = generate_monte_carlo_parameters(atm_conditions, prop_params, engine_params, rocket_params);
         
-        [max, flightdata, forces, Roc, Eng, Prop] = runsim(this_run_atm_conditions, this_run_prop_params, this_run_engine_params, this_run_rocket_params)
-        %%TODO: Output this somewhere besides the command window
-        %results(run,:) = [max, flightdata, forces, Roc, Eng, Prop]
+        [max, flightdata, forces, Roc, Eng, Prop] = runsim(this_run_atm_conditions, this_run_prop_params, this_run_engine_params, this_run_rocket_params);
+        
+        results(runNum) = max.alt;
         
     end
 elseif (mode == 3)
     error('Range of values is not yet implemented');
 end
 
+
+
+
 %% Output
 
-% From https://www.mathworks.com/matlabcentral/answers/2603-add-a-new-excel-sheet-from-matlab
-% Connect to Excel
-Excel = actxserver('excel.application');
-% Get Workbook object
-WB = Excel.Workbooks.Open(fullfile(pwd, 'simconfig.xlsx'), 0, false);
-Excel.Visible = true;
-% Get Worksheets object
-WS = WB.Worksheets;
-% Add after the last sheet
-WS.Add([], WS.Item(WS.Count));
-WS.Item(WS.Count).Name = ['Results ' strrep(datestr(now()), ':', '-')];
-
-% https://stackoverflow.com/questions/7636567/write-information-into-excel-after-each-loop
-WB.Sheets.Item(WS.Count).Activate();
-Excel.Range('A1:E1').Select();
-Excel.Selection.Value = {'Time (s)', 'Acceleration (m/s)', 'Velocity (m/s)', 'Altitude (m)', 'Mach Number'};
-[rows, ~] = size(flightdata);
-Excel.Range(sprintf('A2:E%i', rows)).Select();
-Excel.Selection.Value = num2cell(flightdata);
-
-Excel.Range('F1:H1').Select();
-Excel.Selection.Value = {'Mass (kg)', 'Drag (N)', 'Thrust (N)'};
-[rows, ~] = size(forces);
-Excel.Range(sprintf('F2:H%i', rows)).Select();
-Excel.Selection.Value = num2cell(forces);
-
-% Insert results
-begin_col = 11;
-[~, plusc] = insert_struct_into_excel(max, 'Maximums', Excel, [1, begin_col]);
-begin_col = begin_col + plusc + 2;
-[~, plusc] = insert_struct_into_excel(Prop, 'Propellants', Excel, [1, begin_col]);
-begin_col = begin_col + plusc + 2;
-[~, plusc] = insert_struct_into_excel(Eng, 'Engine', Excel, [1, begin_col]);
-begin_col = begin_col + plusc + 2;
-[~, plusc] = insert_struct_into_excel(Roc, 'Rocket', Excel, [1, begin_col]);
-begin_col = begin_col + plusc + 2;
-
-% Save
-WB.Save();
-% Quit Excel
-Excel.Quit();
+if (mode == 1)
+    % From https://www.mathworks.com/matlabcentral/answers/2603-add-a-new-excel-sheet-from-matlab
+    % Connect to Excel
+    Excel = actxserver('excel.application');
+    % Get Workbook object
+    WB = Excel.Workbooks.Open(fullfile(pwd, 'simconfig.xlsx'), 0, false);
+    Excel.Visible = true;
+    % Get Worksheets object
+    WS = WB.Worksheets;
+    % Add after the last sheet
+    WS.Add([], WS.Item(WS.Count));
+    WS.Item(WS.Count).Name = ['Results ' strrep(datestr(now()), ':', '-')];
+    
+    % https://stackoverflow.com/questions/7636567/write-information-into-excel-after-each-loop
+    WB.Sheets.Item(WS.Count).Activate();
+    Excel.Range('A1:E1').Select();
+    Excel.Selection.Value = {'Time (s)', 'Acceleration (m/s)', 'Velocity (m/s)', 'Altitude (m)', 'Mach Number'};
+    [rows, ~] = size(flightdata);
+    Excel.Range(sprintf('A2:E%i', rows)).Select();
+    Excel.Selection.Value = num2cell(flightdata);
+    
+    Excel.Range('F1:H1').Select();
+    Excel.Selection.Value = {'Mass (kg)', 'Drag (N)', 'Thrust (N)'};
+    [rows, ~] = size(forces);
+    Excel.Range(sprintf('F2:H%i', rows)).Select();
+    Excel.Selection.Value = num2cell(forces);
+    
+    % Insert results
+    begin_col = 11;
+    [~, plusc] = insert_struct_into_excel(max, 'Maximums', Excel, [1, begin_col]);
+    begin_col = begin_col + plusc + 2;
+    [~, plusc] = insert_struct_into_excel(Prop, 'Propellants', Excel, [1, begin_col]);
+    begin_col = begin_col + plusc + 2;
+    [~, plusc] = insert_struct_into_excel(Eng, 'Engine', Excel, [1, begin_col]);
+    begin_col = begin_col + plusc + 2;
+    [~, plusc] = insert_struct_into_excel(Roc, 'Rocket', Excel, [1, begin_col]);
+    begin_col = begin_col + plusc + 2;
+    
+    % Save
+    WB.Save();
+    % Quit Excel
+    Excel.Quit();
+    
+elseif (mode == 2)
+    hist(results);
+end
