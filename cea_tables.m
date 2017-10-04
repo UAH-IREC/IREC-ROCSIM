69e536ea6840dd06f5a24141b5897473b4efc425
moresemicolons <31354662+moresemicolons@users.noreply.github.com>
2017-10-04 15:39:38 -0500

    High-fidelity CEA interpolation
    High-fidelity tables and the script that generated them (cea_tables.m)

---
+++ cea_tables.m
@@ -0,0 +1,10 @@
+tic
+psi2pa = 6894.76; % convert psi to Pa
+eps = 3.5:.25:4.5;
+MRs = 6:.05:10;
+temp_f = [265, 285, 305];
+temp_ox = [269, 289, 309];
+pressures = psi2pa * [200, 300, 400, 500, 600, 700];
+interpolated = CEA_5d_table_gen(pressures, MRs, eps, temp_ox, temp_f);
+save_CEA_table(pressures, MRs, eps, temp_ox, temp_f, interpolated);
+toc
