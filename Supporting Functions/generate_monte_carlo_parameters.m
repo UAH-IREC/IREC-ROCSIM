27a969ea02ce62f1c951288c78dde2335014d5a0
moresemicolons <31354662+moresemicolons@users.noreply.github.com>
2017-10-10 20:41:31 -0500

    Outputs varied fields

--- Supporting Functions/generate_monte_carlo_parameters.m
+++ Supporting Functions/generate_monte_carlo_parameters.m
@@ -1,4 +1,4 @@
-function [ this_run_atm_conditions, this_run_prop_params, this_run_engine_params, this_run_rocket_params] = generate_monte_carlo_parameters(atm_conditions, prop_params, engine_params, rocket_params)
+function [ this_run_atm_conditions, this_run_prop_params, this_run_engine_params, this_run_rocket_params, variedtable] = generate_monte_carlo_parameters(atm_conditions, prop_params, engine_params, rocket_params)
 %GENERATE_MONTE_CARLO_PARAMETERS Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -8,11 +8,13 @@ this_run_engine_params = [];
 this_run_rocket_params = [];
 
 fields = fieldnames(atm_conditions);
+variedtable = [];
 for i = 1:numel(fields)
     if ( length(atm_conditions.(fields{i})) == 1)
         this_run_atm_conditions.(fields{i}) = atm_conditions.(fields{i});
     elseif ( length(atm_conditions.(fields{i})) == 2)
         this_run_atm_conditions.(fields{i}) = normrnd(atm_conditions.(fields{i})(1),atm_conditions.(fields{i})(2));
+        variedtable = [variedtable; [{fields{i}}, this_run_atm_conditions.(fields{i})]];
     end
 end
 
@@ -22,6 +24,7 @@ for i = 1:numel(fields)
         this_run_engine_params.(fields{i}) = engine_params.(fields{i});
     elseif ( length(engine_params.(fields{i})) == 2)
         this_run_engine_params.(fields{i}) = normrnd(engine_params.(fields{i})(1),engine_params.(fields{i})(2));
+        variedtable = [variedtable; [{fields{i}}, this_run_engine_params.(fields{i})]];
     end
 end
 
@@ -39,6 +42,7 @@ for i = 1:numel(fields)
                 this_run_prop_params.(fields{i}).(fields2{j}) = prop_params.(fields{i}).(fields2{j});
             elseif ( length(prop_params.(fields{i}).(fields2{j})) == 2)
                 this_run_prop_params.(fields{i}).(fields2{j}) = normrnd(prop_params.(fields{i}).(fields2{j})(1),prop_params.(fields{i}).(fields2{j})(2));
+                variedtable = [variedtable; [{strcat(fields{i}, '.', fields2{j})}, this_run_prop_params.(fields{i}).(fields2{j})]];
             end
         end
     end
@@ -51,6 +55,7 @@ for i = 1:numel(fields)
         this_run_rocket_params.(fields{i}) = rocket_params.(fields{i});
     elseif ( length(rocket_params.(fields{i})) == 2)
         this_run_rocket_params.(fields{i}) = normrnd(rocket_params.(fields{i})(1),rocket_params.(fields{i})(2));
+        variedtable = [variedtable; [{fields{i}}, this_run_rocket_params.(fields{i})]];
     end
 end
 
