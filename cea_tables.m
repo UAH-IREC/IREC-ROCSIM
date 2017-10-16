ef6ca16e22f0b84c552f8ffbd5e1b4333991a4a7
moresemicolons <31354662+moresemicolons@users.noreply.github.com>
2017-10-16 16:44:26 -0500

    Wider CEA interpolation ranges

--- cea_tables.m
+++ cea_tables.m
@@ -1,10 +1,10 @@
 tic
 psi2pa = 6894.76; % convert psi to Pa
-eps = 3.75:.25:5.0;
-MRs = 6:.05:10;
+eps = 3.75:.25:6.0;
+MRs = 3.5:.05:14;
 temp_f = [265, 285, 305];
 temp_ox = [269, 289, 309];
-pressures = psi2pa * [200, 300, 400, 500, 600, 700];
+pressures = psi2pa * [200, 300, 400, 500, 600];
 interpolated = CEA_5d_table_gen(pressures, MRs, eps, temp_ox, temp_f);
 save_CEA_table(pressures, MRs, eps, temp_ox, temp_f, interpolated);
 toc
